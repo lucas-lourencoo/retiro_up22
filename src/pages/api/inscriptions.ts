c0c3fcc4b5d839bd47276c41dc0aafd4066c561c
Lucas <lucascelestiano@gmail.com>
2022-06-20 13:29:21 -0400

    updated

--- src/pages/api/inscriptions.ts
+++ src/pages/api/inscriptions.ts
@@ -4,14 +4,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const prisma = new PrismaClient();
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Invalid method' });
+  if (req.method === 'POST') {
+    try {
+      const inscription = await prisma.inscriptions.create({ data: req.body });
+      return res.json(inscription);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
   }
 
-  try {
-    const inscription = await prisma.inscriptions.create({ data: req.body });
-    return res.json(inscription);
-  } catch (err) {
-    return res.status(500).send(err.message);
+  if (req.method === 'GET') {
+    try {
+      const inscription = await prisma.inscriptions.findMany({});
+      return res.json(inscription);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
   }
+
+  return res.status(405).json({ message: 'Invalid method' });
 };
